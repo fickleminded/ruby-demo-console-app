de9a4713a22b8a73ed0e027a4ec9be747e6049c6
Tirthankar Bhattacharjee <feignfroggie@gmail.com>
2015-07-09 14:49:44 -0400

    edit code for simplicity

--- deaf-grandma.rb
+++ deaf-grandma.rb
@@ -3,34 +3,17 @@
 # if i scream "BYE", gransma answers "BYE SONNY, SEE YA LATER!" and program quits
 
 
-# begin
-# print "say something: "
-# answer = gets.chomp.strip
-# if answer.downcase == answer
-#   puts "HUH?! SPEAK UP, SONNY!"
-# elsif answer == "BYE"
-#   puts "BYE SONNY, SEE YA LATER!"
-# else
-#   puts "NO, NOT SINCE 1933!"
-# end
-# end until answer == "BYE"
+begin
 
-def get_user_input
   print "say something: "
   answer = gets.chomp.strip
-end
 
-def speak_to_grandma(user_input)
-  if user_input.downcase == user_input
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif user_input == "BYE"
-    return "BYE SONNY, SEE YA LATER!"
+  if answer.downcase == answer
+    puts "HUH?! SPEAK UP, SONNY!"
+  elsif answer == "BYE"
+    puts "BYE SONNY, SEE YA LATER!"
+  else
+    puts "NO, NOT SINCE 1933!"
   end
-  "NO, NOT SINCE 1933!"
-end
 
-def runner
-  begin
-   
-  end until answer == "BYE"
-end
+end until answer == "BYE"
